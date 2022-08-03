5fe08b61b5a2bd7cb5d05d8bddb3b02f9ecddc01
andriy-lanyk <LAM1988@ukr.net>
2022-08-03 18:14:00 +0300

    Improve disabled button

--- src/js/disabledButton.js
+++ src/js/disabledButton.js
@@ -1,11 +1,18 @@
 import { ANIMATION_DURATION } from "./constants";
 
+let timerId;
+
 function disabledButton(number = 0) {
   const startButton = document.querySelector(".start__button");
   startButton.setAttribute("disabled", "disabled");
+
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+
   if (number) {
     const duration = ANIMATION_DURATION * number;
-    setTimeout(() => {
+    timerId = setTimeout(() => {
       startButton.removeAttribute("disabled");
     }, duration);
   }
